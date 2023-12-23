74d053ab3578cf55a1ef3eff5f1569ab5c091c58
mikegrekov <svg200914@gmail.com>
2023-12-23 00:06:39 +0000

    inicial: implementation

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,27 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const re = /^[0-9]+\.*[0-9]*/gm;
+
+  if (typeof +sampleActivity !== 'number'
+    || +sampleActivity < 1
+    || +sampleActivity > 15
+    || typeof sampleActivity !== 'string'
+    || sampleActivity.match(re) === null) return false;
+  const n = +sampleActivity;
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const t = Math.log(MODERN_ACTIVITY / n) / k;
+  return Math.ceil(t);
 }
 
 module.exports = {
   dateSample
 };
+
+
+
+// console.log(dateSample('3'), 13305);
+// console.log(dateSample('1'), 22387);
+// console.log(dateSample('9'), 4223);
+// console.log(dateSample('11'), 2564);
