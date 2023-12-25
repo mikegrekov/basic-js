1b0dc99d7d0ddbd70f35d259d77bd25be7d76d06
mikegrekov <svg200914@gmail.com>
2023-12-25 23:09:42 +0000

    deploy

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error("'arr' parameter must be an instance of the Array!");
+  }
+  if (arr.includes('--discard-next') || arr.includes('--discard-prev') || arr.includes('--double-next') || arr.includes('--double-prev')) {
+    let duplicate = [];
+    arr.forEach(element => duplicate.push(element));
+    let flagChanges = false;
+    for (let i = 0; i < duplicate.length; i++) {
+      if (duplicate[i] === '--discard-next') {
+        if (duplicate[i + 1] != undefined && flagChanges === false) {
+          duplicate.splice(i--, 2);
+        } else {
+          duplicate.splice(i, 1);
+        }
+        flagChanges = true;
+      }
+      if (duplicate[i] === '--discard-prev') {
+        if (duplicate[i - 1] != undefined && flagChanges === false) {
+          duplicate.splice(i - 1, 2);
+        } else {
+          duplicate.splice(i, 1);
+        }
+        flagChanges = true;
+      }
+      if (duplicate[i] === '--double-next') {
+        if (duplicate[i + 1] != undefined && flagChanges === false) {
+          duplicate.splice(i--, 1, duplicate[i + 2]);
+        } else {
+          duplicate.splice(i--, 1);
+        }
+        flagChanges = false;
+      }
+      if (duplicate[i] === '--double-prev') {
+        if (duplicate[i - 1] != undefined && flagChanges === false) {
+          duplicate.splice(i--, 1, duplicate[i - 1]);
+        } else {
+          duplicate.splice(i--, 1);
+        }
+        flagChanges = false;
+      }
+    }
+    return duplicate;
+  } else {
+    return arr;
+  }
 }
 
 module.exports = {
